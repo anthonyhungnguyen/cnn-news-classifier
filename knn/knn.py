0655bbf64c0911f4e8e787bfe3a505d71d6ac471
Anthony Hưng Nguyễn <anthonyhungnguyen276@gmail.com>
2019-03-25 22:50:38 +0700

    fix bugs and increase performance

--- knn/knn.py
+++ knn/knn.py
@@ -1,13 +1,23 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
+def label(cate):
+    return {
+        0: 'business',
+        1: 'entertainment',
+        2: 'health',
+        3: 'sports'
+    }.get(cate, 4)
+
 # Get dataset
 dataset = pd.read_csv('tf_idf.csv')
-X = dataset.iloc[:, 0:(len(dataset)-1)].values
+X = dataset.iloc[:, 0:-1].values
 y = dataset.iloc[:, -1].values
+
+# New data
+dataset_new = pd.read_csv('tf_idf_new.csv')
+X_new = dataset_new.iloc[:, :].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
@@ -20,6 +30,12 @@ clf.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = clf.predict(X_test)
+
+# Predict newtext
+y_new = clf.predict(X_new)
+
+for y in y_new:
+    print(label(y))
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
--- tf-idf/tf-idf.py
+++ tf-idf/tf-idf.py
@@ -1,9 +1,9 @@
 import os
-
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 dir_path = os.path.dirname(os.path.realpath('__file__'))
+
 
 def label(cate):
     return {
@@ -13,32 +13,49 @@ def label(cate):
         'sport': 3
     }.get(cate, 4)
 
+
 listFolder = ['business', 'entertainment', 'health', 'sport']
+
 
 def count_number_of_files(cate):
     return len(next(os.walk("{}\\{}".format(dir_path, cate)))[2])
+
 
 def get_all_filelists():
     file_list = []
     for cate in listFolder:
         num_of_files = count_number_of_files(cate)
-        file_list.extend([dir_path + "\\" + cate + "\\" + str(i) + ".txt" for i in range(0, num_of_files - 1)])
+        file_list.extend([dir_path + "\\" + cate + "\\" +
+                          str(i) + ".txt" for i in range(0, num_of_files)])
     return file_list
+
 
 def exportTFIDF():
     filelist = get_all_filelists()
-    tf_idf =  TfidfVectorizer(input="filename").fit_transform(filelist).toarray()
-    return tf_idf
+    tf_idf_vec = TfidfVectorizer(input="filename").fit(filelist)
+    tf_idf = tf_idf_vec.transform(filelist).toarray()
+    return tf_idf, tf_idf_vec
+
 
 def label_tf_idf():
-    tf_idf = exportTFIDF()
+    tf_idf, tf_idf_vec = exportTFIDF()
     lb = [[]]
     for cate in listFolder:
         num_of_files = count_number_of_files(cate)
         label_num = label(cate)
-        label_matrix = np.full((1, num_of_files - 1), label_num, dtype=int)
+        label_matrix = np.full((1, num_of_files), label_num, dtype=int)
         lb = np.append(lb, label_matrix, axis=1)
-    tf_idf =  np.append(tf_idf, np.matrix.transpose(lb), axis=1)
-    np.savetxt('tf_idf.csv',tf_idf,fmt='%.5f', delimiter=',')
+    tf_idf = np.append(tf_idf, np.matrix.transpose(lb), axis=1)
+    np.savetxt('tf_idf.csv', tf_idf, fmt='%.5f', delimiter=',')
 
+
+def transform_newdata():
+    tf_idf, tf_idf_vec = exportTFIDF()
+    filelist = [dir_path + "\\" + 'newtext' + "\\" +
+                str(i) + ".txt" for i in range(0, count_number_of_files('newtext'))]
+    tf_idf_newtext = tf_idf_vec.transform(filelist).toarray()
+    np.savetxt('tf_idf_new.csv', tf_idf_newtext, fmt='%.5f', delimiter=',')
+
+
+transform_newdata()
 label_tf_idf()
